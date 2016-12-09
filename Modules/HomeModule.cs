1dd0f2c683ec782caa55d0b1ba076b162093017f
Loren Glenn <lorencglenn@gmail.com>
2016-12-09 11:46:01 -0800

    add basic frontend functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -18,6 +18,11 @@ namespace Salon
         List<Client> allClients = Client.GetAll(parameters.id);
         return View["clients.cshtml", allClients];
       };
+
+      Get["/client/{id}"] = parameters => {
+        Client foundClient = Client.Find(parameters.id);
+        return View["client.cshtml", foundClient];
+      };
     }
   }
 }
